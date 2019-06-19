2ad20e37789141292bdb21dbba21a128bfeab043
Larry <DoubleSlashDesign@Gmail.com>
2019-06-19 12:00:15 -0400

    animated screen counter on load

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -23,7 +23,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
         duration: Duration(seconds: 1),
         vsync:
             this, // Using this keyword states that we are pointing back to the welcomeScreenState
-        );
+        upperBound: 100.0);
 
     controller.forward();
     controller.addListener(() {
@@ -36,7 +36,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.red.withOpacity(controller.value),
+      backgroundColor: Colors.white,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 24.0),
         child: Column(
@@ -53,7 +53,7 @@ class _WelcomeScreenState extends State<WelcomeScreen>
                   ),
                 ),
                 Text(
-                  'Flash Chat',
+                  '${controller.value.toInt()}%',
                   style: TextStyle(
                     fontSize: 45.0,
                     fontWeight: FontWeight.w900,
